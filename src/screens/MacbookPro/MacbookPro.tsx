429608fcd963c3a22ba1046d8d938f6bd2384600
ntwarigloria <160240278+ntwarigloria@users.noreply.github.com>
2025-10-24 13:36:03 +0200

    resolved car

--- src/screens/MacbookPro/MacbookPro.tsx
+++ src/screens/MacbookPro/MacbookPro.tsx
@@ -75,9 +75,9 @@ export const MacbookPro = (): JSX.Element => {
             </div>
 
             {/* Center - Image */}
-            <div className="flex-shrink-0 mx-0 md:mx-8">
+            <div className="flex-shrink-0 mx-0 lg:py-16 md:mx-8">
               <img
-                className="w-full max-w-[320px] sm:max-w-[420px] md:max-w-[460px] lg:w-auto lg:h-[420px] object-contain lg:scale-y-150 lg:scale-x-125"
+                className="w-full max-w-[320px] sm:max-w-[420px] md:max-w-[460px] lg:w-auto object-contain lg:scale-125"
                 alt="Central illustration"
                 src="/car_group.svg"
               />
