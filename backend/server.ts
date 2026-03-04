a68d8e234841287272a0da3363dbfba810b5a1fe
ntwarigloria <160240278+ntwarigloria@users.noreply.github.com>
2026-03-04 19:43:03 +0200

    production

--- backend/server.ts
+++ backend/server.ts
@@ -18,6 +18,7 @@ const allowedOrigins = process.env.FRONTEND_URL
   : [
       'http://localhost:5173',
       'https://wheeliz-web.vercel.app',
+      'https://wheeliz-backend.onrender.com/',
       'https://wheeliz-production.up.railway.app'
     ];
 
