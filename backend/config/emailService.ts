ef5a706469f4b59f67b2991d35cacd57d1d1b1a0
ntwarigloria <160240278+ntwarigloria@users.noreply.github.com>
2026-03-07 15:19:25 +0200

    email services

--- backend/config/emailService.ts
+++ backend/config/emailService.ts
@@ -2,8 +2,8 @@ import nodemailer from 'nodemailer';
 
 const transporter = nodemailer.createTransport({
     host: process.env.SMTP_HOST,
-    port: parseInt(process.env.SMTP_PORT || '465'),
-    secure: true, // true for 465, false for other ports
+    port: parseInt(process.env.SMTP_PORT || '587'),
+   secure: false, // true for 465, false for other ports
     auth: {
         user: process.env.SMTP_USER,
         pass: process.env.SMTP_PASS,
