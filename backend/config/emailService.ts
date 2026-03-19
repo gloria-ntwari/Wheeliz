71962f649dd97bac2b72b94bf90b4ff14d288360
ntwarigloria <160240278+ntwarigloria@users.noreply.github.com>
2026-03-19 17:28:02 +0200

    email

--- backend/config/emailService.ts
+++ backend/config/emailService.ts
@@ -9,6 +9,9 @@ const transporter = nodemailer.createTransport({
         user: process.env.SMTP_USER,
         pass: process.env.SMTP_PASS,
     },
+    tls: {
+    rejectUnauthorized: false,
+  },
 });
 
 const FROM_EMAIL = process.env.EMAIL_FROM || process.env.SMTP_USER || '';
